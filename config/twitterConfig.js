69e2e59a489fd642a42090c9c079aba8c6eff231
Jhoterdan de Paula <jotaskt@gmail.com>
2015-02-01 15:30:28 -0200

    update config

--- config/twitterConfig.js
+++ config/twitterConfig.js
@@ -1,8 +1,8 @@
 // insert data of your application (for twitter) 'CONSUMER_KEY', 'CONSUMER_SECRET','TOKEN','TOKEN_SECRET'
 module.exports = {
-	'CONSUMER_KEY'		:'',
-	'CONSUMER_SECRET'	:'',
-	'TOKEN'				:'',
-	'TOKEN_SECRET'		:'',
-	'CONNECTION_STRING' :''
+	'CONSUMER_KEY'		:'ft32ImLd17Wk9cXcI2Nj9Wfqs',
+	'CONSUMER_SECRET'	:'1qhHvpchh2ag3u736pLf0WCsJV1QHOGmQkdcNXlFqcZjJqZ25J',
+	'TOKEN'				:'178430819-6Pey13kXDRJQtEDyUCFf7sNJV6xmq3xlRIgxBm8Q',
+	'TOKEN_SECRET'		:'OynuPzvN2K6t3kMEZX6pD1Pt3qvCCGSHnBHMk2PDKr2mC',
+	'CONNECTION_STRING' :'mongodb://localhost:27017/twitter'
 };
