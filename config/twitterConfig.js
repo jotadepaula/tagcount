0b77e10a0a22b1cd0e5f2abd5dbc3168f4824ae1
Jhoterdan de Paula <jotaskt@gmail.com>
2015-02-01 15:27:16 -0200

    first commit

---
+++ config/twitterConfig.js
@@ -0,0 +1,8 @@
+// insert data of your application (for twitter) 'CONSUMER_KEY', 'CONSUMER_SECRET','TOKEN','TOKEN_SECRET'
+module.exports = {
+	'CONSUMER_KEY'		:'',
+	'CONSUMER_SECRET'	:'',
+	'TOKEN'				:'',
+	'TOKEN_SECRET'		:'',
+	'CONNECTION_STRING' :''
+};
